82244b6cde31efc9ea5f2b91a250ae5966b8c549
Satish-baradiya <baradiyasatish@gmail.com>
2022-01-07 19:53:06 +0530

    Store Completed

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -23,6 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls'))
 ]
-urlpatterns = [
+urlpatterns += static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
 
-]
+
